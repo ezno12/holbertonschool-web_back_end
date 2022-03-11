0de724588c83665c0ee1406ba8e9fb66816ff03e
tennin12 <bassembenyhia12@gmail.com>
2022-03-11 21:36:09 +0100

    f9 fixed

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python3
-"""
-"""
+
+"""Annotated Function"""
 from typing import List, Iterable, Sequence, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """
-    """
+    """Annotated Function"""
     return [(i, len(i)) for i in lst]
