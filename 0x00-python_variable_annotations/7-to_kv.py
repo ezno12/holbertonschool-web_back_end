ea8917b2abddea425682153d4419ac83dd52785c
tennin12 <bassembenyhia12@gmail.com>
2022-03-11 21:04:42 +0100

    f7 fixed

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """
-moduel sum of list.
+moduel tuple.
 """
 from typing import List, Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
-    returns return sum of list elements.
+    returns return tuple.
     """
     return (k, v**2)
