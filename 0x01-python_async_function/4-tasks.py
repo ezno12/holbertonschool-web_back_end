a30b6e587dfb3991dea026cf6275f6a1be1ac75d
tennin12 <bassembenyhia12@gmail.com>
2022-03-12 00:36:33 +0100

    f4 fixed

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -8,7 +8,7 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_random(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     wait
     """
