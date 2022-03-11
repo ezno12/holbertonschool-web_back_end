9c1fb0794c64d325458b353b3560f0e72108ff3e
tennin12 <bassembenyhia12@gmail.com>
2022-03-11 21:44:19 +0100

    f100 fixed

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Annotated Function"""
-from typing import Any, Union
+from typing import Any, Union, Sequence
 
 
-def safe_first_element(lst: Any) -> Union[Any, None]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     if lst:
         return lst[0]
     else:
