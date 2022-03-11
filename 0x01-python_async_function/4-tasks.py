2a3ede1509d1567117d89976abedf973f99c85e5
tennin12 <bassembenyhia12@gmail.com>
2022-03-12 00:33:43 +0100

    f4 added

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+wait modul
+"""
+
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_random(n: int, max_delay: int) -> List[float]:
+    """
+    wait
+    """
+    lst = []
+    for i in range(n):
+        lst.append(await task_wait_random(max_delay))
+    return sorted(lst)
