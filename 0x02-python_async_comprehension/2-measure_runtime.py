597d8eb767853eae0d2acc8d783a07144b661919
tennin12 <bassembenyhia12@gmail.com>
2022-03-12 01:19:05 +0100

    f2 added

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""module"""
+
+import time
+import asyncio
+
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """execute async_comprehension four times"""
+    t1 = time.time()
+    await asyncio.gather(async_comprehension())
+    t2 = time.time()
+    return t2 - t1
