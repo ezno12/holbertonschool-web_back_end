8fd1e37da7210df0f743241ef7f0cc0d5a63809d
tennin12 <bassembenyhia12@gmail.com>
2022-03-18 19:31:13 +0100

    0 app updated

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -1,7 +1,14 @@
+#!/usr/bin/env python3
+"""
+flask app
+"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    """
+    hello world
+    """
     return render_template("0-index.html")
