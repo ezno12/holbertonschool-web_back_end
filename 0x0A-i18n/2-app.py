bee1651e5ae2dca936caaa804902e40b57db0637
tennin12 <bassembenyhia12@gmail.com>
2022-03-19 18:16:09 +0100

    f2 updated

--- 0x0A-i18n/2-app.py
+++ 0x0A-i18n/2-app.py
@@ -9,6 +9,17 @@ app = Flask(__name__)
 babel = Babel(app)
 
 
+class Config(object):
+    """
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
 @babel.localeselector
 def get_locale():
     """
