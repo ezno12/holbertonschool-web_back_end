df342a84005603ad27e825505560c3bc62446bbe
tennin12 <bassembenyhia12@gmail.com>
2022-03-20 02:33:34 +0100

    app 4 added

---
+++ 0x0A-i18n/4-app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+flask app
+"""
+from flask import Flask, request, render_template
+from flask_babel import Babel, gettext
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """
+    config translate
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+"""config app"""
+
+
+@babel.localeselector
+def get_locale():
+    """
+    get local lang
+    """
+    local_lang = request.args.get("locale")
+    supp_lang = app.config["LANGUAGES"]
+    if local_lang in supp_lang:
+        return local_lang
+    else:
+        return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/")
+def hello_world():
+    """
+    hello world
+    """
+    
+    return render_template("4-index.html")
