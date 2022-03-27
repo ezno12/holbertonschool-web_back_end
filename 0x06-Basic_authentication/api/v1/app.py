10620337634a447b4d894fa786010d9bd2e9dc8f
tennin12 <bassembenyhia12@gmail.com>
2022-03-27 23:05:36 +0100

    app task2 added

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -16,7 +16,7 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 @app.errorhandler(401)
 def request_unauthorized(error) -> str:
-    """ Not found handler
+    """ unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
@@ -26,6 +26,14 @@ def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """
+    forbidden handler
+    """
+    return jsonify({"error": "Forbidden"}, 403)
 
 
 if __name__ == "__main__":
