7a6eb78a8aa0bffb8c2fff967f83c1353e6325b7
tennin12 <bassembenyhia12@gmail.com>
2022-03-28 01:14:05 +0100

    authentication dic added

---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+API authentication.
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """
+    authentication class
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        authentication
+        """
+        return False
+    
+
+    def authorization_header(self, request=None) -> str:
+        """
+        authorization
+        """
+        return None
+    
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        current user
+        """
+        return None
