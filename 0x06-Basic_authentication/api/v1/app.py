b1d015131587b699f614e218d6b9a82fd22af160
tennin12 <bassembenyhia12@gmail.com>
2022-04-01 17:36:56 +0100

    ccheck for basic_auth added

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -10,7 +10,7 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
-
+from api.v1.auth.basic_auth import BasicAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -18,7 +18,9 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
 
-if os.getenv('AUTH_TYPE'):
+if os.getenv('AUTH_TYPE') == "basic_auth":
+    auth = BasicAuth()
+else:
     auth = Auth()
 
 
--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from auth import Auth
+from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
