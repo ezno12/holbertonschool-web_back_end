4a4623167fadd5ddcae1139c60bfbc9b34411154
tennin12 <bassembenyhia12@gmail.com>
2022-04-03 06:19:08 +0100

    base auth header class added

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,10 +2,22 @@
 """
 basic auth
 """
+from turtle import st
 from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
     """
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        """
+        if authorization_header is None:
+            return None
+        if isinstance(authorization_header, str) is False:
+            return None
+        if authorization_header[0:6] == "Basic ":
+            return None
+        else:
+            return authorization_header[6:]
