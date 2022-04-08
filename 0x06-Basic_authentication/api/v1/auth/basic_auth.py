37cc492d544f229e4456bdbdce2ddd6716c0f3d7
tennin12 <bassembenyhia12@gmail.com>
2022-04-08 03:49:27 +0100

    base64 auth added

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,16 +2,16 @@
 """
 basic auth
 """
+import base64
 from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
-    """
-    """
-    def extract_base64_authorization_header(self,
-                                            authorization_header: str) -> str:
-        """
-        """
+    """"""
+
+    def extract_base64_authorization_header(self, authorization_header: str
+                                            ) -> str:
+        """"""
         if authorization_header is None:
             return None
         elif isinstance(authorization_header, str) is False:
@@ -20,3 +20,17 @@ class BasicAuth(Auth):
             return None
         else:
             return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """"""
+        if base64_authorization_header is None:
+            return None
+        if isinstance(base64_authorization_header, str) is False:
+            return None
+        try:
+            return base64.b64decode(base64_authorization_header
+                                    ).decode("utf-8")
+        except Exception:
+            return None
