cb635c9c1b2127638fb0253ba32ebc828a8a5809
tennin12 <bassembenyhia12@gmail.com>
2022-05-08 19:28:26 +0100

    class doc fixed

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -8,15 +8,11 @@ from uuid import uuid4
 
 
 class SessionAuth(Auth):
-    """
-    session authentication class inheret it from class Auth
-    """
+    """session authentication class inheret it from class Auth"""
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """
-        method to create session for user by creating user id with sessions id
-        """
+        """method to create session for user by creating user id with sessions id"""
         if user_id is None or type(user_id) is not str:
             return None
         else:
