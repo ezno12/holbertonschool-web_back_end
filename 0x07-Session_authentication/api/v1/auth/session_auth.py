097a4fa3698142592d1883659d3d2e83357a9a5a
tennin12 <bassembenyhia12@gmail.com>
2022-05-08 03:38:14 +0100

    session auth fixed

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """
+session authentication moduel
 """
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """
+    session authentication class
     """
     pass
