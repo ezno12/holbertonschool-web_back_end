a7b8ca8d7ff310ed8eba19c68ead3fbed21503c3
tennin12 <bassembenyhia12@gmail.com>
2022-05-16 16:06:36 +0100

    user session fixed

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -20,7 +20,7 @@ class SessionAuth(Auth):
             return None
         else:
             sessionID = str(uuid4())
-            self.user_id_by_session_id = {sessionID: user_id}
+            self.user_id_by_session_id[sessionID] = user_id
             return sessionID
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
