9ccd6196c5f93688a394241e8cd433e16309ff07
tennin12 <bassembenyhia12@gmail.com>
2022-05-20 20:24:36 +0100

    before request updated and new path added

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -70,11 +70,6 @@ def before_req() -> None:
 
     if (
         auth.authorization_header(request) is None
-        or auth.session_cookie(request) is None
-    ):
-        abort(401)
-    if (
-        auth.authorization_header(request) is None
         and auth.session_cookie(request) is None
     ):
         abort(401)
