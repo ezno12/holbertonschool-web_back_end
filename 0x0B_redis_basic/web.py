9dc3ab32c6847ed371e9203e6950fae47d941835
tennin12 <bassembenyhia12@gmail.com>
2022-06-08 10:11:43 +0100

    implement web cache tracker added

---
+++ 0x0B_redis_basic/web.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+web cache and tracker 
+"""
+import redis
+import requests
+r = redis.Redis()
+count = 0
+
+
+def get_page(url: str) -> str:
+    """
+    obtain the HTML content of a particular URL
+    """
+    r.set(url, count)
+    response = requests.get(url)
+    r.incr(f"count:{url}")
+    r.setex(url, 10, r.get(url))
+    return response.text
+
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
