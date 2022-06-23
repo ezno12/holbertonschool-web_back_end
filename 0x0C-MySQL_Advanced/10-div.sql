d31ed70f1de86c7ef50ae03b44bac16b73fe40e1
tennin12 <bassembenyhia12@gmail.com>
2022-06-23 16:54:34 +0100

    function safe dev added

---
+++ 0x0C-MySQL_Advanced/10-div.sql
@@ -0,0 +1,12 @@
+-- function SafeDiv that divides (and returns)
+-- the first by the second number or 0
+DELIMITER //
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
+BEGIN
+IF b != 0 THEN
+RETURN a / b;
+ELSE
+RETURN 0;
+END;
+DELIMITER;
