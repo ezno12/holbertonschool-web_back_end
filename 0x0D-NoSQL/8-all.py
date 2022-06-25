5757994c281aae007e4a9265c924a4bf82d42958
tennin12 <bassembenyhia12@gmail.com>
2022-06-25 20:30:29 +0100

    list all python func added

---
+++ 0x0D-NoSQL/8-all.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" modul to list all doc in collection using pymongo"""
+
+
+def list_all(mongo_collection):
+    """
+    list all documents in collection
+    """
+    if mongo_collection is None:
+        return []
+    return mongo_collection.find({})
