d5ab1e1e5ad4b81d89c7dc616a78f7317e3ae653
tennin12 <bassembenyhia12@gmail.com>
2022-07-05 15:46:35 +0100

    finally msg updated

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 export default function handleResponseFromAPI(promise) {
   return promise.then(() => ({ status: 200, body: 'success' }))
     .catch(() => Error())
-    .finally(() => { console.log('The fake API is not working currently'); });
+    .finally(() => { console.log('Got a response from the API'); });
 }
