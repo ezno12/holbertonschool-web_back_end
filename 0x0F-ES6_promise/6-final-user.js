0f90fe3b4e8bda21b570118d58a9a04cb7798c7f
tennin12 <bassembenyhia12@gmail.com>
2022-07-05 17:30:25 +0100

    task 6 fixed

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -3,11 +3,13 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-  // eslint-disable-next-line consistent-return
     .then((data) => {
-      if (data[0] === 'fulfilled') {
-        return data;
-      }
-    })
-    .catch((err) => console.log(err));
+        for (const res of data) {
+            if (res.status === 'rejected') {
+                res.value = `Error: ${res.reason.message}`;
+                return res.reason;
+              }
+        }
+    return data;
+    });
 }
