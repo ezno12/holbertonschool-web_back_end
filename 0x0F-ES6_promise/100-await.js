133418dcfef2c90a7012fa131c1b385af1140ff6
tennin12 <bassembenyhia12@gmail.com>
2022-07-05 18:12:30 +0100

    100 added

---
+++ 0x0F-ES6_promise/100-await.js
@@ -0,0 +1,18 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function(){
+    let obj ={
+        photo: null,
+        user: null,
+    }
+    try {
+        obj = {
+            photo: await uploadPhoto(),
+            user: await createUser(),
+        };
+        return obj
+    } catch (error) {
+        return obj
+    }
+
+}
