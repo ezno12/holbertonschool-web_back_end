6fb00992a882befaa7dcc1ac321f5a43ad5921a9
tennin12 <bassembenyhia12@gmail.com>
2022-07-05 16:28:35 +0100

    task 3 fixed

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -2,6 +2,6 @@ import { createUser, uploadPhoto } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser])
-    .then((resualt) => console.log(`${resualt[0].body} ${resualt[1].firstName} ${resualt[1].lastName}`))
-    .catch(() => console.log('Signup system offline'));
+    .then((resualt) => { console.log(`${resualt[0].body} ${resualt[1].firstName} ${resualt[1].lastName}`); })
+    .catch(() => { console.log('Signup system offline'); });
 }
