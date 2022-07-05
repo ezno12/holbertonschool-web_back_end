2b9745076519d1b15d6b512891806a213fffd79e
tennin12 <bassembenyhia12@gmail.com>
2022-07-05 16:10:16 +0100

    resolve all promises and console log added

---
+++ 0x0F-ES6_promise/3-all.js
@@ -0,0 +1,7 @@
+import { createUser, uploadPhoto } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser])
+    .then((resualt) => console.log(`${resualt[0].body} ${resualt[1].firstName} ${resualt[1].lastName}`))
+    .catch(() => console.log('Signup system offline'));
+}
