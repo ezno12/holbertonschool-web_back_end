81265e73b2fe5cd3ead8e35bb9775800590896ed
tennin12 <bassembenyhia12@gmail.com>
2022-07-15 20:02:20 +0100

    task added

--- 0x0E-ES6_basic/6-string-interpolation.js
+++ 0x0E-ES6_basic/6-string-interpolation.js
@@ -1,12 +1,3 @@
-export default function getSanFranciscoDescription() {
-  const year = 2017;
-  const budget = {
-    income: '$119,868',
-    gdp: '$154.2 billion',
-    capita: '$178,479',
-  };
+import getBudgetObject from './7-getBudgetObject.js';
 
-  return (`As of ${year}, it was the seventh-highest income county in the United States\
-, with a per capita personal income of ${budget.income}. As of 2015, San Francisco \
-proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`);
-}
+console.log(getBudgetObject(400, 700, 900));
