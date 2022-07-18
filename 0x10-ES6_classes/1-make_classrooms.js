5d04bfaa7b595d0c2b84d4a23de771eeb3fe3332
tennin12 <bassembenyhia12@gmail.com>
2022-07-18 10:41:36 +0100

    task 1 fixed

--- 0x10-ES6_classes/1-make_classrooms.js
+++ 0x10-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,5 @@
-import ClassRoom from './0-classroom'
-let num = [19, 20, 34]
+import ClassRoom from './0-classroom';
 
-export default function initializeRooms(num) {
-    return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)]
+export default function initializeRooms() {
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
