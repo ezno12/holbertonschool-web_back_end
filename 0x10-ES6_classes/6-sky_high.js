81b9b9cbf6974238ad43da56b68f91031d4d8d9d
tennin12 <bassembenyhia12@gmail.com>
2022-07-19 13:46:31 +0100

    fixed

--- 0x10-ES6_classes/6-sky_high.js
+++ 0x10-ES6_classes/6-sky_high.js
@@ -2,7 +2,6 @@ import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-    if (typeof (sqft) !== 'number' || typeof (floors) !== 'number') throw TypeError('must be a number');
     super(sqft);
     this._floors = floors;
   }
