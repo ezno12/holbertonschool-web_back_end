2a031b0497c4e98aa2f3a7e7de60d17187077f51
tennin12 <bassembenyhia12@gmail.com>
2022-07-19 13:54:33 +0100

    fixed

--- 0x10-ES6_classes/3-currency.js
+++ 0x10-ES6_classes/3-currency.js
@@ -1,29 +1,26 @@
 export default class Currency {
   constructor(code, name) {
-    if (typeof (code) !== 'string' || typeof (name) !== 'string') throw TypeError('need to be a string');
     this._code = code;
     this._name = name;
-  }
-
-  get name() {
-    return this._name;
   }
 
   get code() {
     return this._code;
   }
 
-  set name(name) {
-    if (typeof (name) !== 'string') throw TypeError('need to be a string');
-    this._name = name;
+  get name() {
+    return this._name;
   }
 
   set code(code) {
-    if (typeof (code) !== 'string') throw TypeError('need to be a string');
     this._code = code;
   }
 
+  set name(name) {
+    this._name = name;
+  }
+
   displayFullCurrency() {
-    return `${this._name} (${this._code})`;
+    return `${this._name} (${this.code})`;
   }
 }
