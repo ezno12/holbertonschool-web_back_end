254d3c93ee8f463eb5003efa2f1e53b1e86fc5ff
tennin12 <bassembenyhia12@gmail.com>
2022-07-22 16:42:37 +0100

    check for string added

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,5 @@
 export default function cleanSet(set, startString) {
+  if (startString.length === 0) return '';
   const res = [];
   for (const elem of set) {
     if (elem && elem.startsWith(startString)) {
