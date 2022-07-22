f24d85484cd68dcb22e9667c98e0d42440ebc893
tennin12 <bassembenyhia12@gmail.com>
2022-07-22 12:32:43 +0100

    update students grade added

---
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,16 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  students.filter((student) => student.location === city)
+    .map((item) => {
+      const newRecord = { ...item };
+
+      const newStudent = newGrades.find((student) => student.studentId === item.id);
+
+      if (newStudent) {
+        newRecord.grade = newStudent.grade;
+      } else {
+        newRecord.grade = 'N/A';
+      }
+
+      return newRecord;
+    });
+}
