520bcd4ac8ce2b7038103795b5699e56a1963649
tennin12 <bassembenyhia12@gmail.com>
2022-07-26 15:42:56 +0100

    style fixed

--- 0x11-ES6_data_manipulation/5-typed_arrays.js
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,8 @@
-export default function(length, position, value){
-    const viewData = new DataView(new ArrayBuffer(length))
-    if (position > length){
-        throw new Error("Position outside range")
-    }
-    viewData.setInt8(position, value)
-    return viewData
+export default function (length, position, value) {
+  const viewData = new DataView(new ArrayBuffer(length));
+  if (position > length) {
+    throw new Error('Position outside range');
+  }
+  viewData.setInt8(position, value);
+  return viewData;
 }
